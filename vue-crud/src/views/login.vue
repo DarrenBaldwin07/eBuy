b0c7ed0266f0a740c05e1f807b90f566bb5212c4
Darren B <68653294+Devd0@users.noreply.github.com>
2021-11-21 22:09:02 -0500

    loging page styling changes - (test commit)

--- vue-crud/src/views/login.vue
+++ vue-crud/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='flex flex-row justify-center'>
-    <div class='hidden bg-white h-screen w-2/5 md:flex'>Login</div>
-    <div class='flex flex-col items-center md:m-auto mt-24'>
+    <div class='hidden bg-gradient-to-b from-primary1 to-primary3  h-screen w-2/5 lg:flex'></div>
+    <div class='flex flex-col items-center lg:m-auto mt-24'>
       <img :src="logo" alt="logo">
       <auth class='md:mt-0'/>
     </div>
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+// - Find whats best for you - Easy payment process - Quick delivery
 import logo from '../assets/images/Logo.png'
 import auth from '../components/Auth.vue'
 export default {
